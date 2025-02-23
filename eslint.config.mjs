6272348fd05cb710cca24a34e73534734a88f736
j.sugi.0604@gmail.com <j.sugi.0604@gmail.com>
2025-02-23 12:56:32 +0900

    back

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react/no-unescaped-entities": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
